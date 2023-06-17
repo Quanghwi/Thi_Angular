f1f43740f3ed748554c8274b89e05ef3d4e58e85
Quanghwi <huydqph24532@fpt.edu.vn>
2023-06-17 16:29:57 +0700

    sign in

--- src/app/component/auth/signin/signin.component.ts
+++ src/app/component/auth/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) {
+
+  }
+  signinForm = this.fb.group({
+    email: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+  HandleSubmit() {
+    if (this.signinForm.invalid) return;
+    this.authService.signin(this.signinForm.value).subscribe((user) => {
+      // console.log(user.user.id);
+      if (user.user.id == 1) {
+        this.router.navigate(['/admin'])
+        setTimeout(() => {
+          alert('Đăng nhập thành công')
+        }, 200);
+      } else {
+        this.router.navigate(['/']);
+      }
+
+    })
+  }
 
 }
--- src/app/component/service/auth.service.ts
+++ src/app/component/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,9 @@ export class AuthService {
   constructor(private http: HttpClient) { }
   signUp(user: any) {
     return this.http.post('http://localhost:3000/signup', user)
+  }
+
+  signin(user: any): Observable<any> {
+    return this.http.post<any>(`http://localhost:3000/signin`, user)
   }
 }
